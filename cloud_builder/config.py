addbbe9b2b5ee4743ccbdfba1d3d812319ea29d7
Nexit1312 <nico.merschen@icloud.com>
2026-08-13 22:34:30 +0200

    Add WikiClicks cloud builder config

---
+++ cloud_builder/config.py
@@ -0,0 +1,7 @@
+DUMP_DATE = "20260801"
+DUMP_BASE = f"https://dumps.wikimedia.org/dewiki/{DUMP_DATE}"
+PARTS = [
+    "dewiki-20260801-pages-articles1.xml-p1p297012.bz2",
+    "dewiki-20260801-pages-articles2.xml-p297013p1262093.bz2"
+]
+ANALYTICS_BASE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/de.wikipedia.org/all-access"

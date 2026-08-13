8aea8bd005e5e588fb60f9de1b03e8fe6bdecfe2
Nexit1312 <nico.merschen@icloud.com>
2026-08-13 22:35:29 +0200

    Add full dewiki dump configuration

---
+++ cloud_builder/config_full.py
@@ -0,0 +1,19 @@
+DUMP_DATE = "20260801"
+DUMP_BASE = f"https://dumps.wikimedia.org/dewiki/{DUMP_DATE}"
+PARTS = [
+"dewiki-20260801-pages-articles1.xml-p1p297012.bz2",
+"dewiki-20260801-pages-articles2.xml-p297013p1262093.bz2",
+"dewiki-20260801-pages-articles3.xml-p1262094p2762093.bz2",
+"dewiki-20260801-pages-articles3.xml-p2762094p3376257.bz2",
+"dewiki-20260801-pages-articles4.xml-p3376258p4876257.bz2",
+"dewiki-20260801-pages-articles4.xml-p4876258p6115464.bz2",
+"dewiki-20260801-pages-articles5.xml-p6115465p7615464.bz2",
+"dewiki-20260801-pages-articles5.xml-p7615465p9115464.bz2",
+"dewiki-20260801-pages-articles5.xml-p9115465p9261244.bz2",
+"dewiki-20260801-pages-articles6.xml-p9261245p10761244.bz2",
+"dewiki-20260801-pages-articles6.xml-p10761245p12261244.bz2",
+"dewiki-20260801-pages-articles6.xml-p12261245p13761244.bz2",
+"dewiki-20260801-pages-articles6.xml-p13761245p13942242.bz2",
+]
+ANALYTICS_BASE = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/de.wikipedia.org/all-access"
+USER_AGENT = "WikiClicksCloudBuilder/1.0"
